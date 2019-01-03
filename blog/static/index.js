12044b640488ac6ec1ed3dace0426811f71ccd45
nichijou <angusbike@gmail.com>
2019-01-03 14:32:27 +0800

    new contents style

--- blog/static/index.js
+++ blog/static/index.js
@@ -1,26 +1,28 @@
-const main = document.getElementsByTagName('main')[0];
+const main = document.getElementsByTagName('main')[0]
 
 const cache = {
-  '/api/tutor': ['<img src="/img/工事中アイコン.svg" width="150px" style="margin-top:80px"/>', 'Tutors | nichijou'],
-  '/api/about': [`<h2 style="font-size:50px;color:#eee">ABOUT</h2><pre><code class='language-perl'>my %info = qw{
-                 name angus_zhang
-              twitter <a href="https://twitter.com/nichijou_lab" target="_blank">nichijou_lab</a>
-                email angusbike@gmail.com
-                 desc flag必中型命格，眼高手低
-                motto 自律，不装，兼爱
+  '/api/tutor': ['<object type="image/svg+xml" data="/img/工事中アイコン.svg" width="150px" style="margin-top:80px">Your browser does not support SVG</object>', 'Tutors | nichijou'],
+  '/api/about': [`<pre class="infopre">my %info = qw{<span style="font-size:1.2em;color:#555;">
+              name angus_zhang
+           twitter <a href="https://twitter.com/nichijou_lab" target="_blank">nichijou_lab</a>
+             email angusbike@gmail.com
+              desc flag必中型命格，眼高手低
+             motto 自律，不装，兼爱</span>
              };
 
-my %site_info = (
-                 front_end => [qw| HTML CSS Javascript        |],
-                  back_end => [qw| Nodejs Express MongoDB     |],
-                    credit => [qw| Org-js Prismjs GoogleFonts |],
-                      desc => '<a href='/post/TYZOX7'>link</a>',
-                repository => '<a href='https://github.com/whitemuu/nodeblog' target='_blank'>whitemuu/nodeblog</a>'
-                );</code></pre><div></div>`, 'About | nichijou']
+my %site_info = (<span style="font-size:1.2em;color:#555;">
+              front_end => [qw| HTML CSS Javascript        |],
+               back_end => [qw| Nodejs Express MongoDB     |],
+                 credit => [qw| Org-js Prismjs GoogleFonts |],
+                   desc => '<a href='/post/TYZOX7'>link</a>',
+             repository => '<a href='https://github.com/whitemuu/nodeblog' target='_blank'>whitemuu/nodeblog</a>'</span>
+                );</pre>`, 'About | nichijou']
 }
 // age ${new Date().getFullYear() - 1991}
 
 function genTagsHtml(tags){
+  // TODO
+  if (tags === undefined) return ''
   return '<div class="tags">' + tags.split(' ').reduce((sum, tag) => `${sum}<span id='${tag}'>${tag}</span>`,'') + '</div>'
 }
 
@@ -31,7 +33,7 @@ function genCreated(name) {
 const bindSectionJump = () =>
       Array.from(document.getElementsByClassName("section-number")).forEach(sectionNumber => {
         sectionNumber.onclick = () =>
-          document.getElementById('header-' + sectionNumber.innerText.replace(/\./g,"-")).scrollIntoView({behavior: 'smooth', block: 'start'});
+          document.getElementById('header-' + sectionNumber.innerText.replace(/\./g,"-")).scrollIntoView({behavior: 'smooth', block: 'start'})
       })
 
 function f404(){
@@ -43,12 +45,12 @@ function route(path) {
   if(path) window.history.pushState(null, null, path)
   else path = window.location.pathname
 
-  let hit = cache['/api' + path];
+  let hit = cache['/api' + path]
 
   if (hit) {
-    main.innerHTML = hit[0];
-    document.title = hit[1];
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    main.innerHTML = hit[0]
+    document.title = hit[1]
+    window.scrollTo({ top: 0, behavior: 'smooth' })
     return
   }
 
@@ -63,12 +65,14 @@ function route(path) {
         document.title = 'Posts | nichijou'
         cache['/api/posts'] = []
         cache['/api/posts'][1] = 'Posts | nichijou'
-        const contents = posts.reduce((sum, post) => {
+
+        let contents = posts.reduce((sum, post) => {
           let path = `/post/${post.title.replace(/ /g, '-')}-${parseInt(post.name.substr(0, 10)).toString(36)}`
-          return `${sum}<div class="entry"><h1 style="font:lighter 1.5em sans-serif;margin-top:2em;">
-<a href="${path}" onclick="route('${path}'); return false">${post.title}</a></h1>
-<div class="date">${genCreated(post.name)}</div>
-${genTagsHtml(post.tags)}</div>`},'')
+          return `${sum}<span>\n  (${genCreated(post.name)} '(<a href="${path}" style="font-size:1.5em" onclick="route('${path}'); return false">${post.title}</a>)
+              :tags ${genTagsHtml(post.tags)})</span>`},'')
+
+        contents = `<pre class="infopre">(contents${contents || '\n  nil'})</pre>`
+
         main.innerHTML = contents
         cache['/api/posts'][0] = contents
         // window.scrollTo({ top: 0, behavior: 'smooth' })
@@ -90,7 +94,7 @@ ${genTagsHtml(post.tags)}</div>`},'')
         } , {})
 
         tags = Object.keys(tags).reduce((sum, tag) => `${sum}<span id="${tag}" style="font-size:${15 + tags[tag] * 2}px">${tag}</span>`, '')
-        // console.log(tags);
+        // console.log(tags)
         tags = `<div id="tags-container" class='tags' style="margin-top:80px">${tags}</div>`
         main.innerHTML = tags
 
@@ -111,9 +115,9 @@ ${genTagsHtml(post.tags)}</div>`},'')
     ;(async () => {
       hit = cache[url]
       if (hit) {
-        main.innerHTML = hit[0];
-        document.title = hit[1];
-        window.scrollTo({ top: 0, behavior: 'smooth' });
+        main.innerHTML = hit[0]
+        document.title = hit[1]
+        window.scrollTo({ top: 0, behavior: 'smooth' })
       } else {
         try {
           const res = await fetch(url)
@@ -124,7 +128,9 @@ ${genTagsHtml(post.tags)}</div>`},'')
           cache[url] = []
           cache[url][1] = post.title
           let content = `<h1>${post.title}</h1>${genTagsHtml(post.tags)}
-<div id="meta">Created: <a href="${post.html_url}" target="_blank">${genCreated(post.name)}</a> by Angus Zhang</div>
+<div id="meta"><a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">
+ <img alt="Creative Commons License" style="border-width:0;opacity:0.5" src="https://i.creativecommons.org/l/by-nc-nd/4.0/80x15.png" /></a>
+Created: <a href="${post.html_url}" target="_blank">${genCreated(post.name)}</a> by Angus Zhang</div>
 ${post.content}<div id='eof'>✣</div>`
           let newPath = `/post/${post.title.replace(/ /g, '-')}-${parseInt(post.name.substr(0, 10)).toString(36)}`
           if (window.location.pathname !== newPath) {
